241a1d19c9f6e1bec30873de639744afb394723a
user <email@example.com>
2016-02-19 12:01:40 +0100

    -

--- app/src/main/java/onion/blog/PostActivity.java
+++ app/src/main/java/onion/blog/PostActivity.java
@@ -2,6 +2,7 @@
  * Blog.onion
  *
  * http://play.google.com/store/apps/details?id=onion.blog
+ * http://onionapps.github.io/Blog.onion/
  * http://github.com/onionApps/Blog.onion
  *
  * Author: http://github.com/onionApps - http://jkrnk73uid7p5thz.onion - bitcoin:1kGXfWx8PHZEVriCNkbP5hzD15HS4AyKf
